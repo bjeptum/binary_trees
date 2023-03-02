8c95e96fc23dfa50798267ad1446ed8d88d29059
b_jeptum <brendajeptum@gmail.com>
2023-03-02 08:18:20 -0800

    Updated height of tree

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -13,11 +13,14 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	if (tree == NULL)
 		return (0);
 
+	if(tree->left == NULL && tree->right == NULL)
+		return (0);
+
 	lheight = binary_tree_height(tree->left);
 	rheight = binary_tree_height(tree->right);
 
 	if (lheight > rheight)
-		return (lheight + 1);
+		return (lheight) + 1;
 	else
-		return (rheight + 1);
+		return (rheight) + 1;
 }
