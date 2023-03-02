578c8bde482b09dd8f0a0eb464083474e6351a92
b_jeptum <brendajeptum@gmail.com>
2023-03-02 12:21:31 -0800

    Updated header

--- binary_trees.h
+++ binary_trees.h
@@ -40,7 +40,7 @@ size_t binary_tree_nodes(const binary_tree_t *tree);
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_sibling(binary_tree_t *node);
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
-
+int binary_tree_is_leaf(const binary_tree_t *node);
 
 
 #endif/*binary_trees.h*/
