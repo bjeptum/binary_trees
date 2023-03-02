7304bf53ae2f222665d7c109ee8aada65ec03ff7
b_jeptum <brendajeptum@gmail.com>
2023-03-02 12:15:47 -0800

    Insert left node

--- binary_trees.h
+++ binary_trees.h
@@ -38,6 +38,8 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 size_t binary_tree_height(const binary_tree_t *tree);
 size_t binary_tree_nodes(const binary_tree_t *tree);
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
+binary_tree_t *binary_tree_sibling(binary_tree_t *node);
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
 
 
 
