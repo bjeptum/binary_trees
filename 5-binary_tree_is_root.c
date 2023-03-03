a85fcd24df3fea042392df12ba4f5ec28eaa8edb
b_jeptum <brendajeptum@gmail.com>
2023-03-03 05:55:20 -0800

    Check node is root

--- 5-binary_tree_is_root.c
+++ 5-binary_tree_is_root.c
@@ -10,8 +10,6 @@ int binary_tree_is_root(const binary_tree_t *node)
 		return (0);
 	if (node->parent != NULL)
 		return (0);
-	else if (node->parent != NULL)
-		return (0);
 	else
 		return (1);
 }
