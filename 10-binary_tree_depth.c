76ca92ca94b93f996b8a32feafcf4fffb1e56582
b_jeptum <brendajeptum@gmail.com>
2023-03-03 07:53:30 -0800

    Depth of a node

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -2,21 +2,18 @@
 /**
  * binary_tree_depth - Measures the depth of a node in a binary tree
  * @tree: pointer to the node to measure the depth
- * Return: 0
+ * Return: depth, else 0
  */
 size_t binary_tree_depth(const binary_tree_t *tree)
 {
 	if (!tree)
 		return (0);
-	else
+
+	size_t l_Depth = 0;
+
+	if (tree->parent)
 	{
-		size_t l_Depth = 0;
-		if (tree->parent)
-		{
-			l_Depth = 1 + binary_tree_depth(tree->parent);
-		}
-		else
-			l_Depth = 0;
-		return (l_Depth);
+		l_Depth = 1 + binary_tree_depth(tree->parent);
 	}
+	return (l_Depth);
 }
